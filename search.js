8b824ec5e336de418d8e45ce2265c279adb1847d
kamiyalog <genkiterui0896@gmail.com>
2026-06-20 18:34:26 +0900

    Add files via upload

--- search.js
+++ search.js
@@ -1,11 +1,7 @@
 function search() {
   const q = document.getElementById("searchBox").value.trim();
 
-  if (q === "AG-1234") {
-    window.location.href = "data.html";
-  } else if (q === "送迎") {
-    window.location.href = "transport.html";
-  } else if (q === "緑馬宗司") {
+  if (q === "緑馬宗司") {
     window.location.href = "profile-soji.html";
   } else if (q === "ベーカリーあかむね") {
     window.location.href = "thread-archive.html";
@@ -13,23 +9,22 @@ function search() {
     window.location.href = "registry-ryoko.html";
   } else if (q === "早川ベーカリー") {
     window.location.href = "hayakawa-bakery.html";
-} else if (q === "東雲大学") {
-  window.location.href = "shinonome-award.html";
-} else if (q === "明坂朱里") {
-  window.location.href = "member-akari.html";
-} else if (q === "会員登録") {
-  window.location.href = "member-register.html";
-} else if (q === "会員限定予約") {
-  window.location.href = "member-reservation.html";
-} else if (q === "最上送迎") {
-  window.location.href = "mogami-transport.html";
-} else if (q === "AA-5381") {
-  window.location.href = "adaptive-data.html";
-} else if (q === "東雲生体適応研究所") {
-  window.location.href = "shinonome-research.html";
-} else if (q === "食品依存形成機序") {
-  window.location.href = "dependency-report.html";
-
+  } else if (q === "東雲大学") {
+    window.location.href = "shinonome-award.html";
+  } else if (q === "明坂朱里") {
+    window.location.href = "member-akari.html";
+  } else if (q === "会員登録") {
+    window.location.href = "member-register.html";
+  } else if (q === "会員限定予約") {
+    window.location.href = "member-reservation.html";
+  } else if (q === "最上送迎") {
+    window.location.href = "mogami-transport.html";
+  } else if (q === "AA-5381") {
+    window.location.href = "adaptive-data.html";
+  } else if (q === "東雲生体適応研究所") {
+    window.location.href = "shinonome-research.html";
+  } else if (q === "食品依存形成機序") {
+    window.location.href = "dependency-report.html";
   } else {
     alert("該当データは存在しません");
   }
